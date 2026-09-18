314acb0721f04956b7489e6acac89a01d366e6d3
Juniorcontrole68 <junior.controle68@gmail.com>
2026-09-18 09:57:06 -0300

    Inicializa módulo de Controle de Coletas

--- contas-a-pagar-v3/server.js
+++ contas-a-pagar-v3/server.js
@@ -2,6 +2,7 @@ const http = require('http');
 const { loadEnv } = require('./src/utils');
 loadEnv();
 const { initDb } = require('./src/db');
+const { initColetasDb } = require('./src/coletas');
 const { handler } = require('./src/handler');
 
 const PORT = process.env.PORT || 3000;
@@ -10,9 +11,9 @@ if (!process.env.DATABASE_URL) {
   process.exit(1);
 }
 
-initDb().then(() => {
+Promise.all([initDb(), initColetasDb()]).then(() => {
   http.createServer(handler).listen(PORT, '0.0.0.0', () => {
-    console.log(`Contas A Pagar v3 ativo na porta ${PORT}`);
+    console.log(`Contas A Pagar v3 + Controle de Coletas ativo na porta ${PORT}`);
   });
 }).catch(err => {
   console.error('Falha ao iniciar banco:', err);
